c1dbc683a5b9a6010f1972d9b197a912d7bdd138
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2025-03-27 15:46:13 +0200

    added core lib

---
+++ Abjjad.Images.Core/IEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace Abjjad.Images.Core;
+
+public interface IEntity<TId>
+{
+    TId Id { get; set; }
+}
---
+++ Abjjad.Images.Core/Models/Enhancement.cs
@@ -0,0 +1,11 @@
+﻿using Abjjad.Images.Core.Enums;
+
+namespace Abjjad.Images.Core.Models;
+
+public class Enhancement : Entity<Guid>
+{
+    public string RequestId { get; set; }
+    public ImagePath OriginalImagePath { get; set; }
+    public Dictionary<ImageSize, ImagePath> ResizedImagePaths { get; set; } = new Dictionary<ImageSize, ImagePath>();
+    public Dictionary<string, string> Metadata { get; set; } = new Dictionary<string, string>();
+}
